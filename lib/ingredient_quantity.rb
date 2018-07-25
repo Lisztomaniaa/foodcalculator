f2255374e621f4fd52f612fda4ae1b64d0989a18
Lisztomaniaa <lizontik@gmail.com>
2018-07-25 23:52:53 +0300

    fixed

--- lib/ingredient_quantity.rb
+++ lib/ingredient_quantity.rb
@@ -1,5 +1,26 @@
-class IngredientQuantity
+class IngredientQuantity < Ingredient
 
   attr_reader :ingredient, :quantity
 
+  def initialize (ingredient, quantity)
+    @ingredient = ingredient
+    @quantity = quantity
+  end
+
+  def sum(other)
+    if other.ingredient = @ingredient
+      IngredinetQuantity.new(@ingredient, other.quantity + @quantity)
+    else
+      raise ArgumentError
+    end
+  end
+
+  def *(other)
+    IngredinetQuantity.new(@ingredient, @quantity * other)
+  end
+
+   def total_cost
+     total_price ||= 0
+     total_price = @quantity * @ingredient.cost
+   end
 end
