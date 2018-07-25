fbd37627d966c6dab80fe16a034c1be74a2c7247
Lisztomaniaa <lizontik@gmail.com>
2018-07-25 20:07:01 +0300

    done

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,3 +1,15 @@
+
 class Ingredient
-attr_reader :name, :cost
+  attr_reader :name, :cost
+
+  def initialize (name, cost)
+    @name = name
+    @cost = cost
+  end
+
+  def ==(other)
+  @name == other.name && @cost == other.cost
+  end
+
+
 end
